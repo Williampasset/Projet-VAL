0832e9bb890e747571c589576d042bcaaad41e44
Williampasset <william.passet@student.junia.com>
2023-11-17 15:52:49 +0100

    Ajoutez des fichiers projet.

---
+++ Projet-VAL/Projet-VAL.cpp
@@ -0,0 +1,12 @@
+﻿// Projet-VAL.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Projet-VAL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Projet-VAL/Projet-VAL.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <iostream>
+
+class Rame {
+private:
+	int nbpassager;
+	//Position
+
+};
