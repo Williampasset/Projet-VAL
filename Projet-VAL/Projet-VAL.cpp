2801a96f2d7d16cb1134f9ea08434326d9ae240c
Williampasset <william.passet@student.junia.com>
2023-11-20 08:05:34 +0100

    commit projet journée 2

--- Projet-VAL/Projet-VAL.cpp
+++ Projet-VAL/Projet-VAL.cpp
@@ -1,12 +1,41 @@
-﻿// Projet-VAL.cpp : définit le point d'entrée de l'application.
-//
-
-#include "Projet-VAL.h"
+﻿#include "Projet-VAL.h"
+#include "vector"
+#include "algorithm"
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
+void avancer(std::stop_token stop_token, Rame rame) {
+	while (!stop_token.stop_requested()) {
+		std::this_thread::sleep_for(100ms);
+		auto distance = rame.getDistanceDArame();
+		distance += (0.083) * distance;
+		rame.setDistanceDA(distance);
+		std::cout << "distance rame :  " << distance << std::endl;
+	}
+}
+
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	vector<Rame> rames;
+	vector<Station> stations;
+	Rame rame(1);
+	//Rame rame1(2);
+	//Rame rame2(3);
+	//rames.push_back(rame); 
+	//rames.push_back(rame1);
+	//rames.push_back(rame2);
+	//Station station("Depart", 0); 
+	//Station station1("bois blanc", 500);
+	//Station station2("bois blanc", 1200);
+	//Station station3("arrivee", 1700);
+	//while(rames[0].getDistanceDArame() != station3.getDistanceDAstation()){
+
+	//}
+	std::stop_source s_source;
+	std::jthread thr1(avancer, s_source.get_token(), rame);
+
 	return 0;
 }
